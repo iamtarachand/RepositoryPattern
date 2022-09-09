df563fce7545d25a37dbf153c40d4d507c583cc8
iamtarachand <tarachandmahto1999@gmail.com>
2022-09-09 22:12:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -0,0 +1,130 @@
+package com.example.android.devbyteviewer.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.domain.DevByteVideo
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDomainModel
+import com.example.android.devbyteviewer.repository.VideosRepository
+import kotlinx.coroutines.*
+import java.io.IOException
+
+
+class DevByteViewModel(application: Application) : AndroidViewModel(application) {
+
+    /**
+     * The data source this ViewModel will fetch results from.
+     */
+    // TODO: Add a reference to the VideosRepository class
+    private val videosRepository = VideosRepository(getDatabase(application))
+    /**
+     * A playlist of videos displayed on the screen.
+     */
+    // TODO: Replace the MutableLiveData and backing property below to a reference to the 'videos'
+    // TODO: from the VideosRepository
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                videosRepository.refreshVideos()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                if(playlist.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
+        }
+    }
+    /**
+     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private val _playlist = MutableLiveData<List<DevByteVideo>>()
+
+    /**
+     * A playlist of videos that can be shown on the screen. Views should use this to get access
+     * to the data.
+     */
+    val playlist: LiveData<List<DevByteVideo>>
+        get() = _playlist
+
+    /**
+     * Event triggered for network error. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    /**
+     * Event triggered for network error. Views should use this to get access
+     * to the data.
+     */
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    /**
+     * Flag to display the error message. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    /**
+     * Flag to display the error message. Views should use this to get access
+     * to the data.
+     */
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    /**
+     * init{} is called immediately when this ViewModel is created.
+     */
+    init {
+        // TODO: Replace with a call to the refreshDataFromRepository9) method
+        refreshDataFromNetwork()
+    }
+
+    /**
+     * Refresh data from the repository. Use a coroutine launch to run in a
+     * background thread.
+     */
+    // TODO: Replace with the refreshDataFromRepository() method
+    private fun refreshDataFromNetwork() = viewModelScope.launch {
+        try {
+            val playlist = DevByteNetwork.devbytes.getPlaylist()
+            _playlist.postValue(playlist.asDomainModel())
+
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+
+        } catch (networkError: IOException) {
+            // Show a Toast error message and hide the progress bar.
+            _eventNetworkError.value = true
+        }
+    }
+
+    /**
+     * Resets the network error flag.
+     */
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    /**
+     * Factory for constructing DevByteViewModel with parameter
+     */
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DevByteViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return DevByteViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
